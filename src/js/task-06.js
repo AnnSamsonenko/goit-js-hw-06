4fe36bf9a2d1e5cfb8bc6942dd56c18adcd1a103
Anna Samsonenko <foster.anna93@gmail.com>
2021-10-09 20:00:55 +0300

    add some changes

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -15,15 +15,26 @@
 // РЕШЕНИЕ
 
 const inputRef = document.querySelector("#validation-input");
+const dataValidLength = parseInt(inputRef.dataset.length);
 
 const onValidSimbolsInInput = (event) => {
-  if (event.currentTarget.value.length === 6) {
+  if (event.currentTarget.value.length === dataValidLength) {
     event.currentTarget.classList.add("valid");
     event.currentTarget.classList.replace("invalid", "valid");
   } else {
     event.currentTarget.classList.add("invalid");
-    event.currentTarget.classList.replace("valid", "invalid");
   }
 };
 
 inputRef.addEventListener("blur", onValidSimbolsInInput);
+
+//Этот вариант ментор подсказал))
+
+// const inputRef = document.querySelector("#validation-input");
+// const datasetLength = parseInt(inputRef.dataset.length);
+// inputRef.addEventListener("change", (event) => {
+//   event.target.value.length === datasetLength
+//     ? event.target.classList.remove("invalid") &
+//       event.target.classList.add("valid")
+//     : event.target.classList.add("invalid");
+// });
